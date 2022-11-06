1c4322dae0b7697ca46d5ce01d16025c01dfd12f
John <jps64@njit.edu>
2022-11-06 15:00:35 -0500

    Revert "player movement and rotation done"
    This reverts commit 867be6597a831c7dc143cd9fe11e0b8b3ce10344.
    this is what messed it alll up

--- src/player.c
+++ src/player.c
@@ -39,7 +39,7 @@ void player_think(Entity *self)
     Vector3D right;
     Vector3D up;
     Vector2D j;
-
+    
     const Uint8 * keys;
     keys = SDL_GetKeyboardState(NULL); // get the keyboard state for this frame
 
@@ -50,12 +50,11 @@ void player_think(Entity *self)
     j = vector2d_from_angle(self->camRotate.z);
     camForward.x = j.x;
     camForward.y = j.y;
-    camRight.x = j.y;
-    camRight.y = -j.x;
+    camRight.x = j.x-GFC_HALF_PI;
+    camRight.y = j.y-GFC_HALF_PI;
 
-    vector3d_set_magnitude(&camForward,0.15);
-    vector3d_set_magnitude(&camRight,0.15);
     //CONSTANT GRAVITY
+
     if(self->position.z > 0)
     {
         vector3d_add(self->position,self->position, self->gravity);
@@ -67,21 +66,16 @@ void player_think(Entity *self)
     }
     if (keys[SDL_SCANCODE_S])
     {
-        vector3d_add(self->position,self->position,-camForward);
-        self->rotation.z = vector_angle(camForward.x, camForward.y) * GFC_DEGTORAD;  
-        
+        vector3d_add(self->position,self->position,-camForward);  
+
     }
     if (keys[SDL_SCANCODE_D])
     {
-        vector3d_add(self->position,self->position, camRight); 
-        self->rotation.z = (vector_angle(camRight.x, camRight.y) + 180) * GFC_DEGTORAD;  
-
+        vector3d_add(self->position,self->position,camRight); 
     }
     if (keys[SDL_SCANCODE_A])    
     {
-        vector3d_add(self->position,self->position,-camRight);
-        self->rotation.z = vector_angle(camRight.x, camRight.y) * GFC_DEGTORAD;  
-
+        vector3d_add(self->position,self->position,-right);
     }
     if (keys[SDL_SCANCODE_SPACE] && self->canJump)
     {
@@ -95,20 +89,12 @@ void player_think(Entity *self)
 
     if (keys[SDL_SCANCODE_Z])self->position.z -= 0.10;
     
-    //player melee = e
 
-    //player heavy = q
+    if (keys[SDL_SCANCODE_Q])self->rotation.z -= 0.0050;
+    if (keys[SDL_SCANCODE_E])self->rotation.z += 0.0050;
     
-    
-    //player rotation testing
-    //if (keys[SDL_SCANCODE_Q])self->rotation.z -= 0.0050;
-    //if (keys[SDL_SCANCODE_E])self->rotation.z += 0.0050;
-    
-    //camera pitch movement not needed
-    //if (keys[SDL_SCANCODE_UP])self->camRotate.x -= 0.0050;
-    //if (keys[SDL_SCANCODE_DOWN])self->camRotate.x += 0.0050;    
-    
-    //camera left and right movement
+    if (keys[SDL_SCANCODE_UP])self->camRotate.x -= 0.0050;
+    if (keys[SDL_SCANCODE_DOWN])self->camRotate.x += 0.0050;    
     if (keys[SDL_SCANCODE_RIGHT])self->camRotate.z -= 0.0050;
     if (keys[SDL_SCANCODE_LEFT])self->camRotate.z += 0.0050;
     
@@ -127,7 +113,7 @@ void player_update(Entity *self)
     vector3d_copy(position,self->position);
     vector3d_copy(rotation,self->camRotate);
         position.z += 30;
-        rotation.x += M_PI*0.020;
+        rotation.x += M_PI*0.125;
         w = vector2d_from_angle(self->camRotate.z);
         forward.x = w.x * 100;
         forward.y = w.y * 100;
